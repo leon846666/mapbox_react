5c9e4771db786c40d985b32b6c883e9ec51bd295
Leo <leon846666@gmail.com>
2023-08-11 15:24:30 +1200

    Excluding Mapbox GL JS explicitly from transpilation

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,14 @@ module.exports = {
   webpack: {
     configure: (webpackConfig) => {
       webpackConfig.module.noParse = /(mapbox-gl)\.js$/;
+
+      webpackConfig.module.rules.push({
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto"
+      });
+      
+      
       return webpackConfig;
     }
   }
